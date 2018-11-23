191f4f6c7bf8bada970bf202301e63ee5f9dc2ee
David Rodríguez <deivid.rodriguez@riseup.net>
2018-11-23 08:07:25 +0100

    Initial commit

---
+++ lib/force_ruby_platform_test.rb
@@ -0,0 +1,6 @@
+require "force_ruby_platform_test/version"
+
+module ForceRubyPlatformTest
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/force_ruby_platform_test/version.rb
@@ -0,0 +1,3 @@
+module ForceRubyPlatformTest
+  VERSION = "0.1.0"
+end
